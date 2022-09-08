39c121c56bd664ec2b0f138ee0c83626c69830b9
Ben Davies <bendavies@Bens-MacBook-Pro.local>
2022-09-08 11:36:04 +0100

    kajsbdf

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useRef, useState } from 'react';
 import './App.css';
+import Editor, { useMonaco } from "@monaco-editor/react";
 
 function App() {
+
+  const monaco = useMonaco();
+
+  const [ editorUri, setEditorUri] = useState<string>();
+
+  const tsProxy = useRef<any>();
+  
+
+  useEffect(() => {
+    if(!editorUri) return;
+    monaco?.languages.typescript.getTypeScriptWorker().then((worker: any) => {
+      worker(editorUri).then((proxy: any) => tsProxy.current = proxy);
+    })
+            
+
+  }, [monaco, editorUri])
+
+  useEffect(() => {
+    monaco?.languages.typescript.typescriptDefaults.addExtraLib(`
+      export {};
+      declare global {
+        declare var hello: string;
+      }
+    `);
+  }, [monaco])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Editor
+        language={"typescript"}
+        height="90vh"
+        onMount={(editor: any) => { setEditorUri(editor.getModel().uri)}}
+      
+      >
+    </Editor>
+    <button onClick={() => {
+      const a = tsProxy.current.getEmitOutput(editorUri?.toString()).then((result:any) => console.log(JSON.stringify(result.outputFiles[0].text)))
+    }} >Compile lol</button>
+    </>
+    
   );
 }
 
 export default App;
+
